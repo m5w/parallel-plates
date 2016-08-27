c0a14aefb3804d8784d00280b677313140c9445f
matthew <m5w@users.noreply.github.com>
2016-08-26 22:42:50 -0400

    Reflect derivation, print bold characters for vectors, and print interpuncts for multipliers

--- ParallelPlates.cpp
+++ ParallelPlates.cpp
@@ -22,26 +22,27 @@ int main(int argc, char **argv) {
     throw;
 
   const long double LENGTH = std::stold(argv[1]), WIDTH = std::stold(argv[2]),
-                    HEIGHT_SQUARED = std::pow(std::stold(argv[3]), 2.0l);
+                    HEIGHT = std::stold(argv[3]);
+  const long double HEIGHT_SQUARED = std::pow(HEIGHT, 2.0l);
   const long double DELTA_X_1 = LENGTH / std::pow(2.0l, std::stoull(argv[4])),
                     DELTA_Y_1 = WIDTH / std::pow(2.0l, std::stoull(argv[5])),
                     DELTA_X_2 = LENGTH / std::pow(2.0l, std::stoull(argv[6])),
                     DELTA_Y_2 = WIDTH / std::pow(2.0l, std::stoull(argv[7]));
-  const long double DELTA = DELTA_Y_2 * DELTA_X_2 * DELTA_Y_1 * DELTA_X_1;
+  const long double DELTA = DELTA_X_1 * DELTA_Y_1 * DELTA_X_2 * DELTA_Y_2;
   long double force_z = 0.0l;
 
   for (long double x_1 = 0.0l; x_1 < LENGTH; x_1 += DELTA_X_1) {
     for (long double y_1 = 0.0l; y_1 < WIDTH; y_1 += DELTA_Y_1) {
       for (long double x_2 = 0.0l; x_2 < LENGTH; x_2 += DELTA_X_2) {
         for (long double y_2 = 0.0l; y_2 < WIDTH; y_2 += DELTA_Y_2) {
-          force_z +=
-              DELTA * std::pow(std::pow(x_2 - x_1, 2.0l) +
-                                   std::pow(y_2 - y_1, 2.0l) + HEIGHT_SQUARED,
-                               -1.5l);
+          force_z -= HEIGHT * DELTA /
+                     std::pow(std::pow(x_2 - x_1, 2.0l) +
+                                  std::pow(y_2 - y_1, 2.0l) + HEIGHT_SQUARED,
+                              1.5l);
         }
       }
     }
   }
 
-  std::cout << force_z << '\n';
+  std::cout << "𝐅₁₂ = " << force_z << " · C · 𝐤\n";
 }
